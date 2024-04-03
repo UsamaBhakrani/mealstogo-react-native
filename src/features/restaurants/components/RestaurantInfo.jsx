cb5251404753a6cce320ecdca81feaa8e53bf4d6
Mohammed Usama <usamabhakrani@gmail.com>
2024-04-03 13:46:41 +0500

    Defaults

--- src/features/restaurants/components/RestaurantInfo.jsx
+++ src/features/restaurants/components/RestaurantInfo.jsx
@@ -1,6 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 
-const RestaurantInfo = () => {
+const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [],
+    address = "100 Street, Random Area",
+    isOpenNow = "",
+    rating = 4.5,
+    isClosedTemporarily = true,
+  } = restaurant;
+
   return (
     <View>
       <Text>RestaurantInfo</Text>
